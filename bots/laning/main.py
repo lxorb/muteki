a094bde73b7b2685630ad39a0c32a1b09b6dca18
lxorb <101118850+lxorb@users.noreply.github.com>
2026-03-22 08:37:27 +0100

    add method to find core center

--- bots/laning/main.py
+++ bots/laning/main.py
@@ -20,7 +20,7 @@ add_project_root_to_path()
 
 import random
 import time
-from cambc import Controller, Direction, EntityType
+from cambc import Controller, Direction, EntityType, Position
 from lib.information import Information
 
 
@@ -28,6 +28,8 @@ class Bot:
     def __init__(self):
         self.num_spawned = 0  # number of builder bots spawned so far (core)
         self.information: Information | None = None
+        self.bb_handler = None
+        self.core_center_pos: Position | None = None
 
     def get_ns_elapsed(self):
         return time.perf_counter_ns() - self.t_start
@@ -62,13 +64,58 @@ class Bot:
 
     def run_core(self, ct: Controller):
         # spawn initial bb's
-        if self.num_spawned < INITIAL_BB:
+        if self.num_spawned < len(INITIAL_BB):
             spawn_pos = ct.get_position().add(random.choice(DIRECTIONS))
             if ct.can_spawn(spawn_pos):
                 ct.spawn_builder(spawn_pos)
                 self.num_spawned += 1
 
     def run_bb(self, ct: Controller):
+        if self.core_center_pos is None:
+            self.find_core_center(ct)
+
+        if self.bb_handler is None:
+            self.bb_handler = self.get_initial_bb_handler(ct)
+
+        self.bb_handler(ct)
+
+    def find_core_center(self, ct: Controller) -> Position | None:
+        current_pos = ct.get_position()
+        building_id = ct.get_tile_building_id(current_pos)
+        if (
+            building_id is not None
+            and ct.get_entity_type(building_id) == EntityType.CORE
+            and ct.get_team(building_id) == ct.get_team()
+        ):
+            self.core_center_pos = ct.get_position(building_id)
+            return self.core_center_pos
+
+        for building_id in ct.get_nearby_buildings():
+            if (
+                ct.get_entity_type(building_id) == EntityType.CORE
+                and ct.get_team(building_id) == ct.get_team()
+            ):
+                self.core_center_pos = ct.get_position(building_id)
+                return self.core_center_pos
+
+        return None
+
+    def get_initial_bb_handler(self, ct: Controller):
+        round_index = ct.get_current_round() - 1
+        if 0 <= round_index < len(INITIAL_BB):
+            return INITIAL_BB[round_index].__get__(self, type(self))
+        return self.run_bb_unassigned
+
+    def run_bb_maintainer(self, ct: Controller):
+        pass
+
+    def run_bb_scavenger(self, ct: Controller):
+        pass
+
+    def run_bb_harrassment(self, ct: Controller):
+        pass
+
+    def run_bb_unassigned(self, ct: Controller):
         pass
 
     def run_gunner(self, ct: Controller):
@@ -90,4 +137,14 @@ class Player(Bot):
 
 ### constants ###
 DIRECTIONS = [d for d in Direction if d != Direction.CENTRE]
-INITIAL_BB = 10
+INITIAL_BB = [
+    Bot.run_bb_maintainer,
+    Bot.run_bb_maintainer,
+    Bot.run_bb_maintainer,
+    Bot.run_bb_maintainer,
+    Bot.run_bb_maintainer,
+    Bot.run_bb_maintainer,
+    Bot.run_bb_maintainer,
+    Bot.run_bb_maintainer,
+    Bot.run_bb_maintainer,
+]
