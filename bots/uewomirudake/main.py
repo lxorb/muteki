50725061f4a7a11b41d5a69306ec3adb3977d588
infinitylooped <144592811+infinitylooped@users.noreply.github.com>
2026-03-29 17:58:22 +0200

    fix agent error

--- bots/uewomirudake/main.py
+++ bots/uewomirudake/main.py
@@ -7,13 +7,11 @@ class Player:
         self.agent: Agent | None = None
 
     def run(self, ct: Controller) -> None:
-        # TODO
-
         entity_type: EntityType = ct.get_entity_type()
 
         if self.agent is None:
             match entity_type:
                 case _:
-                    self.agent = Agent
+                    self.agent = Agent()
         
         self.agent.run(ct)
