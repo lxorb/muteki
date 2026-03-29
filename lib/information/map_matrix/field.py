bae6bfbc6b7b42f8c95774b24652e1d1631a631f
einbocha <65666391+einbocha@users.noreply.github.com>
2026-03-29 18:58:46 +0200

    removed unused libraries and refactored core logic by deleting redundant modules and classes

--- lib/information/map_matrix/field.py
+++
@@ -1,9 +0,0 @@
-from enum import StrEnum
-
-
-class Field(StrEnum):
-    __slots__ = ()
-    BUILDABLE = "buildable"
-    NON_BUILDABLE = "non_buildable"
-    CONVEYOR = "conveyor"
-    HARVESTER = "harvester"
